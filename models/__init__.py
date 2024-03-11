956fe2c019e9efc777840b54590c41e2606a6427
kamau95 <linuskamau586@gmail.com>
2024-03-11 12:08:50 -0700

    bnb clone base class

--- models/__init__.py
+++
@@ -1,3 +0,0 @@
-from models.engine.file_storage import FileStorage
-storage = FileStorage("file.json")
-storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -1,49 +1,52 @@
 #!/usr/bin/python3
-"""
-this file contains a base class BaseModel that
-defines all common attributes/methods for other classes
-"""
-
+"""This file holds  the basemodel
+class which is the parent class"""
 
 import uuid
-import datetime
+from datetime import datetime
 
 
 class BaseModel:
-    """this is base class"""
-    time_format = '%Y-%m-%dT%H:%M:%S.%f'
-
+    """my parent class - other classes will inherit"""
+    time_format = "%Y-%m-%dT%H:%M:%S.%f"
     def __init__(self, *args, **kwargs):
-        """initialization of a new BaseModel instance"""
-        if kwargs:
-            self.id = kwargs.get('id', str(uuid.uuid4()))
-            for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
-                if key in ('created_at', 'updated_at'):
-                    value = datetime.datetime.strptime(value, self.time_format)
-                    setattr(self, key, value)
+        """Initializes instance attributes
+        Args:
+            - *args: list of arguments
+            - **kwargs: dict of key-values arguments
+        """
+
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], time_format)
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], time_format)
+                else:
+                    self.__dict__[key] = kwargs[key]
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
-
-    def save(self):
-        """updates attribute updated_at with the current datetime"""
-        self.updated_at = datetime.datetime.now()
-
-    def to_dict(self):
-        """adding new key/value pairs to my object"""
-        obj_dict = self.__dict__.copy()
-
-        """Convert created_at and updated_at to ISO format strings"""
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-
-        """Add __class__ key with the value class name"""
-        obj_dict['__class__'] = self.__class__.__name__
-        return obj_dict
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
-        """overwriting the str method"""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        """Returns string representation"""
+
+        return f"[{self.__class__.__name__}] \
+               ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """updates the public instance attribute updated_at"""
+
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__"""
+
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = self.__class__.__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        return my_dict
