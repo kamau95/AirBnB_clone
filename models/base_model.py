46ff773c5a64268caa30db9eaefb6f43c6ccba54
kamau95 <linuskamau586@gmail.com>
2024-03-11 13:17:28 -0700

    final

--- models/base_model.py
+++ models/base_model.py
@@ -1,54 +1,52 @@
-#!/usr/bin/python3
-"""This file holds  the basemodel
-class which is the parent class"""
-
 import uuid
 from datetime import datetime
 
 
+"""
+this file will hold the parent class
+"""
+
+
 class BaseModel:
-    """my parent class - other classes will inherit"""
-    time_format = "%Y-%m-%dT%H:%M:%S.%f"
-
+    """
+    this class defines the common
+    attributes and mthds of child classes
+    """
     def __init__(self, *args, **kwargs):
-
-        """Initializes instance attributes
-        Args:
-            - *args: list of arguments
-            - **kwargs: dict of key-values arguments
-        """
-
-        if kwargs is not None and kwargs != {}:
-            for key in kwargs:
-                if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                        kwargs["created_at"], BaseModel.time_format)
-                elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(
-                        kwargs["updated_at"], BaseModel.time_format)
-                else:
-                    self.__dict__[key] = kwargs[key]
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-
-    def __str__(self):
-        """Returns string representation"""
-
-        return f"[{self.__class__.__name__}] \
-               ({self.id}) {self.__dict__}"
+        """This method initializes a new BaseModel instance"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key in ("created_at", "updated_at"):
+                    value = datetime.datetime.strptime(
+                            value, "%Y-%m-%dT%H:%M:%S.%f"
+                    )
+                    setattr(self, key, value)
 
     def save(self):
-        """updates the public instance attribute updated_at"""
-
+        """
+        updates the updated_at attribute
+        with the current datetime
+        """
         self.updated_at = datetime.now()
 
-    def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__"""
+    def __str__(self):
+        """
+        returns a string representation
+        """
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__, self.id, self.__dict__
+        )
 
-        my_dict = self.__dict__.copy()
-        my_dict["__class__"] = self.__class__.__name__
-        my_dict["created_at"] = my_dict["created_at"].isoformat()
-        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
-        return my_dict
+    def to_dict(self):
+        """
+        This method returns a dictionary
+        representation of the instance
+        """
+        obj_dict = self.__dict__.copy()
+        obj_dict["__class__"] = self.__class__.__name__
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+        return obj_dict
