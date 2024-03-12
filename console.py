7ff8c1eab387cda8deb22b11c53d59c75defcfed
kamau95 <linuskamau586@gmail.com>
2024-03-12 12:10:22 -0700

    console

---
+++ console.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""this script contains the entry point to the cmd interpreter"""
+
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    simple interactive CLI
+    """
+    prompt = "(hbnb)"
+
+    def do_quit(self, arg):
+        """ Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        return True
+
+    def emptyline(self):
+        """Called when an empty line is entered"""
+        pass
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
